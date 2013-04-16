63049550c23e8c523d09f2eb36b6958151be83f4
talexander <atarakanoff@gmail.com>
2013-04-16 18:59:25 +0400

    prepare admin

---
+++ store/store/admin.py
@@ -0,0 +1,12 @@
+# --*-- coding: utf-8 --*--
+
+
+from django.contrib import admin
+from store import models
+
+admin.site.register(models.Measure, models.AdminMeasure)
+admin.site.register(models.Store, models.AdminStore)
+admin.site.register(models.PropertyGroup, models.AdminPropertyGroup)
+admin.site.register(models.Property, models.AdminProperty)
+admin.site.register(models.GoodCategory, models.AdminGoodCategory)
+admin.site.register(models.Good, models.AdminGood)
--- store/store/settings.py
+++ store/store/settings.py
@@ -135,6 +135,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'store',
 )
 
 # A sample logging configuration. The only tangible logging

e523b79448d0a9d751882507f106f4cea6af5c45
talexander <atarakanoff@gmail.com>
2013-04-19 11:03:56 +0400

    implement __unicode__ for models

--- store/store/fixtures/initial_data.sql
+++ store/store/fixtures/initial_data.sql
@@ -6,3 +6,16 @@ INSERT INTO measure (name, alias, descr) VALUES
 ('мл.', 'ml', 'миллилитр'),
 ('упак.', 'packet', 'упаковка');
 
+INSERT INTO property_group (alias, name) VALUES 
+('common', 'общие')
+;
+
+SET @COMMON_PROP_GROUP_ID = NULL; 
+SELECT @COMMON_PROP_GROUP_ID := id FROM property_group WHERE alias='common';
+
+INSERT INTO property (alias, name, measure, property_group) VALUES 
+('pic', 'Картинка', NULL, @COMMON_PROP_GROUP_ID),
+('tag', 'Тег', NULL, @COMMON_PROP_GROUP_ID)
+--,()
+--,()
+;
--- store/store/models.py
+++ store/store/models.py
@@ -1,12 +1,13 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 from django.db import models
 from django.contrib.admin import ModelAdmin
 
-# Create your models here.
-
 class Store(models.Model):
     name = models.CharField(max_length = 255)
+
+    def __unicode__(self):
+        return u'%s' % self.name
 
     class Meta:
         db_table = 'store'
@@ -19,11 +20,15 @@ class Measure(models.Model):
     descr = models.CharField(max_length = 100)
     name = models.CharField(max_length = 100)
 
+    def __unicode__(self):
+        return u'%s (%s)' % (self.descr, self.name)
+
     class Meta:
         db_table = 'measure'
 
 class AdminMeasure(ModelAdmin):
     list_display = ('display_name', 'alias')
+
     def display_name(self, obj):
         return "%s (%s)" % (obj.descr, obj.name)
     display_name.short_description = 'Name'
@@ -35,29 +40,38 @@ class PropertyGroup(models.Model):
     name = models.CharField(max_length = 255)
     flags = models.BigIntegerField(default = 0)
 
+    def __unicode__(self):
+        return u'%s' % self.name
+
     class Meta:
         db_table = 'property_group'
 
 class AdminPropertyGroup(ModelAdmin):
-    pass
+    list_display = ('name', 'alias')
 
 
 class Property(models.Model):
     alias = models.CharField(max_length = 40)
     name = models.CharField(max_length = 255)
-    measure = models.ForeignKey('Measure', db_column= 'measure') # @TODO: прописать название поля в БД
-    propery_group = models.ForeignKey('PropertyGroup', db_column = 'property_group') # @TODO: прописать название поля в БД
+    measure = models.ForeignKey('Measure', db_column= 'measure', blank = True, null = True)
+    property_group = models.ForeignKey('PropertyGroup', db_column = 'property_group')
     flags = models.BigIntegerField(default = 0)
+
+    def __unicode__(self):
+        return u'%s' % self.name
 
     class Meta:
         db_table = 'property'
 
 class AdminProperty(ModelAdmin):
-    pass
+    list_display = ('name', 'alias', 'measure', 'property_group')
 
 class GoodCategory(models.Model):
     name = models.CharField(max_length = 255)
     flags = models.BigIntegerField(default = 0)
+
+    def __unicode__(self):
+        return u'%s' % self.name
 
     class Meta:
         db_table = 'good_category'
@@ -71,6 +85,9 @@ class Good(models.Model):
     good_category = models.ForeignKey('GoodCategory', db_column = 'good_category')
     descr = models.TextField()
     flags = models.BigIntegerField(default = 0)
+
+    def __unicode__(self):
+        return u'%s' % self.name
 
     class Meta:
         db_table = 'good'
